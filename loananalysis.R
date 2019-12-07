9d08688f3c6da6d515ffc34c55ad1cca7ac26fc4
Patrick <protzetter@bluewin.ch>
2019-12-07 11:19:54 +0100

    clean up code

--- loananalysis.R
+++ loananalysis.R
@@ -7,7 +7,6 @@ library(ggplot2)
 library(dplyr)
 library(factoextra)
 library(FactoMineR)
-library(kohonen)
 library(corrplot)
 require('randomForest')
 require('party')
@@ -27,7 +26,7 @@ loans<-read.csv(textConnection(lines), sep=',',stringsAsFactors = FALSE)
 # lines <- read_csv("LoanStats_2017Q1 2.csv",sep=',')
 # remove some unwanted columns like id, member_id, address, zip
 
-loans<-select(loans, -c(id,member_id,emp_title, annual_inc,zip_code,addr_state))
+loans<-select(loans, -c(id,member_id,emp_title, zip_code,addr_state))
 
 # remove some unwanted columns like url, desc
 
@@ -41,15 +40,22 @@ loans<-select(loans, -c(url,desc,purpose,title,
 loans$int_rate<-as.numeric(gsub("[\\%]", "", loans$int_rate))
 loans$revol_util<-as.numeric(gsub("[\\%]", "", loans$revol_util))
 loans$total_acc<-as.numeric(gsub("[\\%]", "", loans$total_acc))
+
+# get employment length and remove years to get cleaner data
 loans$emp_length<-gsub("[^0-9+<]","",loans$emp_length)
 loans$emp_length<-as.factor(loans$emp_length)
-numCols=c("loan_amnt","funded_amnt", "out_prncp","out_prncp_inv","dti","delinq_2yrs",
+
+
+# identify numeric columns and convert them to numeric
+
+numCols=c("annual_inc","loan_amnt","funded_amnt", "out_prncp","out_prncp_inv","dti","delinq_2yrs",
         "mths_since_last_delinq","mths_since_last_record","earliest_cr_line",
         "inq_last_6mths","total_pymnt","last_pymnt_amnt", "annual_inc_joint","dti_joint","mths_since_last_major_derog")
           
 for (col in numCols) {
   loans[[col]]<-as.numeric(loans[[col]])
 }
+
 
 # remove NA values from known columns so far and replace by 0
 loans$num_accts_ever_120_pd[is.na(loans$num_accts_ever_120_pd)]<-0  
@@ -58,7 +64,8 @@ loans$num_tl_30dpd[is.na(loans$num_tl_30dpd)]<-0
 loans$num_tl_90g_dpd_24m[is.na(loans$num_tl_90g_dpd_24m)]<-0 
 loans$num_tl_op_past_12m[is.na(loans$num_tl_op_past_12m)]<-0 
 loans$delinq_2yrs[is.na(loans$delinq_2yrs)]<-0 
-
+loans$mths_since_last_delinq[is.na(loans$mths_since_last_delinq)]<-0
+loans$mths_since_last_major_derog[is.na(loans$mths_since_last_major_derog)]<-0
 # figure out which columns are numeric so that we can look at the distribution
 numeric_cols <- sapply(loans, is.numeric)
 
@@ -74,16 +81,14 @@ bad_indicators <- c("Charged Off",
                     "Late (16-30 days)",
                     "Late (31-120 days)")
 
-# assign certain statuses to a 'bad' ('0') group
-loans$is_bad <- ifelse(loans$loan_status %in% bad_indicators, 1,
-                          ifelse(loans$loan_status=="", NA, 0)
-)
-
+# assign certain statuses to a 'bad' ('1') group
+loans$is_bad <- ifelse(loans$loan_status %in% bad_indicators, 1,0)
 loans$is_bad<-as.factor(loans$is_bad)
+loans<-select(loans,-loan_status)
 
 
 
-# Let us lok at missing values
+# Let us look at missing values
 # Determine percentage  of NAs in each column
 naPercentage <-sapply(loans, function(y) sum(length(which(is.na(y))))/length(y))
 hist(naPercentage[naPercentage>0], col = "blue", xlab = "Percentange of null values",
@@ -96,7 +101,7 @@ naPercentage <-sapply(loans, function(y) sum(length(which(is.na(y))))/length(y))
 hist(naPercentage[naPercentage>0], col = "blue", xlab = "Percentange of null values",
      main = "Frequency of null values")
 
-
+colnames(loans)[colSums(is.na(loans)) > 0]
 # convert char to factors
 loans <- mutate_if(loans, is.character, as.factor)
 
@@ -116,35 +121,60 @@ hist(loans$installment,col = 'blue', xlab='Installment Amount', main = 'Distribu
 
 # let us look at interest rate by grade
 
-plot(loans$int_rate~loans$grade, loans)
+plot(loans$int_rate~loans$grade, loans,col='blue',xlab = "Loan grade", ylab="Interest rate")
 
-
+# let us look at th enumber of loans by credit sub-grade
 fdc<- loans%>% group_by(sub_grade) %>% summarize(count=n())
-ggplot(fdc,aes(x=sub_grade,y=count))+geom_col()
+ggplot(fdc,aes(x=sub_grade,y=count))+geom_col(fill = "slateblue")+labs(x="Loan sub-grade", y="Count")+theme(
+      panel.border = element_blank(),  
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "grey")
+    )
 
-# let us look at the past due as function of loan grade
+# let us look at the past due as function of loan grade averaged by number of loans
 fdc<- loans%>% group_by(sub_grade) %>% summarize(PDMean=sum(num_accts_ever_120_pd)/n()) %>% arrange(PDMean)
-ggplot(fdc, aes(x=sub_grade, y=PDMean))+geom_col()+theme_minimal()
-fdc<- loans%>% group_by(sub_grade) %>% summarize(PDMean=sum(delinq_2yrs)/n()) %>% arrange(PDMean)
-ggplot(fdc, aes(x=sub_grade, y=PDMean))+geom_col()+theme_minimal()
+ggplot(fdc, aes(x=sub_grade, y=PDMean))+geom_col()+geom_col(fill = "slateblue")+labs(x="Loan sub-grade", y="Past due more than 2 years",title="Percentage of past due by loan sub-grade")+theme(
+  panel.border = element_blank(),  
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.background = element_blank(),
+  axis.line = element_line(colour = "grey")
+)
 
-fdc<- loans%>% group_by(emp_length) %>% summarize(PDMean=sum(num_accts_ever_120_pd+num_tl_120dpd_2m
-                                                            +num_tl_30dpd)/n()) %>% arrange(PDMean)
-ggplot(fdc, aes(x=emp_length, y=PDMean))+geom_col()+theme_minimal()
 
-ggplot(loans,aes(y=loans$num_accts_ever_120_pd,x=loans$sub_grade))+geom_col()
-ggplot(loans,aes(y=loans$mths_since_recent_revol_delinq,x=loans$sub_grade))+geom_col()
+
+fdc<- loans%>% group_by(sub_grade) %>% summarize(delinq=sum(delinq_2yrs)/n()) %>% arrange(delinq)
+ggplot(fdc, aes(x=sub_grade, y=delinq))+geom_col()+geom_col(fill = "slateblue")+labs(x="Loan sub-grade", y="Deliquency in past 2 years",title="Percentage of deliquency by loan sub-grade")+theme(
+  panel.border = element_blank(),  
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.background = element_blank(),
+  axis.line = element_line(colour = "grey")
+)
 
 fdc<- loans%>% group_by(emp_length) %>% summarize(badloan=sum(as.numeric(is_bad)))                                 
-ggplot(fdc, aes(x=emp_length, y=badloan))+geom_col()+theme_minimal()
+ggplot(fdc, aes(x=emp_length, y=badloan))+geom_col(fill = "slateblue")+labs(x="Length of employment", y="Bad loans",title="Number of bad loans by employmentlegth")+
+  theme(
+  panel.border = element_blank(),  
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.background = element_blank(),
+  axis.line = element_line(colour = "grey"))
 
 
-# figure out which columns are numeric so that we can look at the distribution
+# Figure out which columns are numeric so that we can look at the distribution
 numericCols <- sapply(loans, is.numeric)
 nonumeric_cols<- sapply(loans, is.factor)
+#loans<-loans[rowSums(is.na(loans)) == 0,]
 loansnum<-loans[, numericCols]
 loansnonum<-loans[,nonumeric_cols]
+
+colnames(loansnum)[colSums(is.na(loansnum)) > 0]
+loansnum[is.na(loansnum)] <- 0
 # experiment k-means
+
 fullTrainMatrix<-as.matrix(as.data.frame(lapply(loansnum, as.numeric)))
 fullTrainMatrix<-scale(fullTrainMatrix)
 
@@ -174,12 +204,12 @@ for (i in 1:15) {
 }
 
 # Plot total within sum of squares vs. number of clusters
-plot(1:7, wss, type = "b", 
+plot(1:15, wss, type = "b", 
      xlab = "Number of Clusters", 
      ylab = "Within groups sum of squares")
 
 
-cluster<-kmeans(fullTrainMatrix, centers=2, iter.max = 20)
+cluster<-kmeans(fullTrainMatrix, centers=3, iter.max = 20)
 print(cluster)
 fviz_cluster(cluster, data = fullTrainMatrix,
              #palette = c("#00AFBB","#2E9FDF", "#E7B800", "#FC4E07"),
@@ -189,6 +219,7 @@ fviz_cluster(cluster, data = fullTrainMatrix,
 # random forest
 loansnum$isBad<-loans$is_bad
 loansnum[ , "isBad"][is.na(loansnum[ , "isBad"])]<-0
+
 loansnonum$isBad<-loansnum$isBad
 set.seed(101)
 modelRF <- randomForest(isBad ~ ., data=loansnum, ntree=20, importance = TRUE)
@@ -204,7 +235,7 @@ plot_ <- ggplot(Important_Features[1:10,],
                 aes(x= reorder(Feature,
                                Importance) , y = Importance) ) +
   geom_bar(stat = "identity", 
-           fill = "#800080") +
+           fill = "slateblue") +
   coord_flip() +
   theme_light(base_size = 20) +
   xlab("") + 
@@ -219,7 +250,7 @@ loansnonum<-select(loansnonum,-c("verification_status","policy_code"))
 loansnonum<-select(loansnonum,-c("next_pymnt_d"))
 loansnonum<-select(loansnonum,-c("initial_list_status"))
 loansnonum<-select(loansnonum,-is_bad)
-loansnonum<-loansnonum[rowSums(is.na(loansnonum)) == 0,])
+loansnonum<-loansnonum[rowSums(is.na(loansnonum)) == 0,]
 modelRF <- randomForest(isBad ~ ., data=loansnonum, ntree=20, importance = TRUE)
 plot(modelRF$err.rate[, 1], type = "l", xlab = "nombre d'arbres", ylab = "erreur OOB")
 preds<-predict(modelRF, select(loansnonum,-isBad))
@@ -229,7 +260,7 @@ plot(modelRF)
 important <- importance(modelRF, type=1 )
 Important_Features <- data.frame(Feature = row.names(important), Importance = important[, 1])
 Important_Features <-Important_Features[order(-Important_Features$Importance),]
-plot_ <- ggplot(Important_Features[1:5,], 
+plot_ <- ggplot(Important_Features, 
                 aes(x= reorder(Feature,
                                Importance) , y = Importance) ) +
   geom_bar(stat = "identity", 
